804a0a06909321d047a005abbf98542188207351
Jessica Watts <jessperr182@hotmail.com>
2020-09-17 00:18:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,18 @@
 # Add your code here
 class Dog 
-  
+
   @@all []
+  attr_accessor :name, :children
   
   def initialize (name)
     @name = name 
+    @children = []
+    
     @@all = self 
+  end 
+  
+  def self.all
+    @@all 
   end 
   
   
